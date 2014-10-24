bf4d4c71c47d8d9cab97add5de65f90f40945dab
danielsf <scott.f.daniel@gmail.com>
2014-10-24 10:33:05 -0700

    removed unnecessary comments from example

--- examples/findChipNameExample.py
+++ examples/findChipNameExample.py
@@ -14,27 +14,6 @@ from lsst.afw.cameraGeom.cameraFactory import makeCameraFromPath
 from lsst.afw.table import AmpInfoCatalog
 from astropy.io import fits
 
-"""
-Using cameraGeomExample.py in afw, we ought to be able to make an LSST-like
-camera object
-
-However, most of the methods behind findChipName assume that you are calling it
-from a catalog object (with all of the attendant MJD, Site, dbobj.epoch
-variables available.
-
-Something must be done to make the method more general.
-
-split the method up
-
-one method will perform the calculation.  It will accept an ObservationMetaData
-object, a Site object, and a reference epoch
-
-the other method will be a getter that will actually perform the calculations
-
-I suspect we should just split all of the astrometry methods
-
-"""
-
 def convertAmpNames(rawName):
    name = rawName.replace(':','')
    name = name.replace(',','')
