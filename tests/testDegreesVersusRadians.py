1cd40ed965cb81623c3c5d8f467a66f3a38b4880
danielsf <scott.f.daniel@gmail.com>
2015-09-10 16:35:42 -0700

    flesh that test out

--- tests/testDegreesVersusRadians.py
+++ tests/testDegreesVersusRadians.py
@@ -12,6 +12,10 @@ class AstrometryDegreesTest(unittest.TestCase):
         self.raList = numpy.random.random_sample(self.nStars)*2.0*numpy.pi
         self.decList = (numpy.random.random_sample(self.nStars)-0.5)*numpy.pi
         self.mjdList = numpy.random.random_sample(10)*5000.0 + 52000.0
+        self.pm_raList = radiansFromArcsec(numpy.random.random_sample(self.nStars)*10.0 - 5.0)
+        self.pm_decList = radiansFromArcsec(numpy.random.random_sample(self.nStars)*10.0 - 5.0)
+        self.pxList = radiansFromArcsec(numpy.random.random_sample(self.nStars)*2.0)
+        self.v_radList = numpy.random.random_sample(self.nStars)*500.0 - 250.0
 
 
     def testApplyPrecession(self):
@@ -32,22 +36,19 @@ class AstrometryDegreesTest(unittest.TestCase):
 
 
     def testApplyProperMotion(self):
-        pm_ra = radiansFromArcsec(numpy.random.random_sample(self.nStars)*10.0 - 5.0)
-        pm_dec = radiansFromArcsec(numpy.random.random_sample(self.nStars)*10.0 - 5.0)
-        px = radiansFromArcsec(numpy.random.random_sample(self.nStars)*2.0)
-        v_rad = numpy.random.random_sample(self.nStars)*500.0 - 250.0
+
 
         for mjd in self.mjdList:
             raRad, decRad = coordUtils._applyProperMotion(self.raList, self.decList,
-                                                          pm_ra, pm_dec, px, v_rad,
-                                                          mjd=mjd)
+                                                          self.pm_raList, self.pm_decList,
+                                                          self.pxList, self.v_radList, mjd=mjd)
 
             raDeg, decDeg = coordUtils.applyProperMotion(numpy.degrees(self.raList),
                                                          numpy.degrees(self.decList),
-                                                         arcsecFromRadians(pm_ra),
-                                                         arcsecFromRadians(pm_dec),
-                                                         arcsecFromRadians(px),
-                                                         v_rad, mjd=mjd)
+                                                         arcsecFromRadians(self.pm_raList),
+                                                         arcsecFromRadians(self.pm_decList),
+                                                         arcsecFromRadians(self.pxList),
+                                                         self.v_radList, mjd=mjd)
 
             numpy.testing.assert_array_almost_equal((raRad-self.raList)/(numpy.radians(raDeg)-self.raList),
                                                     numpy.ones(self.nStars), 9)
@@ -55,6 +56,20 @@ class AstrometryDegreesTest(unittest.TestCase):
             numpy.testing.assert_array_almost_equal((decRad-self.decList)/(numpy.radians(decDeg)-self.decList),
                                                     numpy.ones(self.nStars), 9)
 
+
+        for ra, dec, pm_ra, pm_dec, px, v_rad in \
+        zip(self.raList, self.decList, self.pm_raList, self.pm_decList, \
+        self.pxList, self.v_radList):
+
+            raRad, decRad = coordUtils._applyProperMotion(ra, dec, pm_ra, pm_dec, px, v_rad,
+                                                          mjd=self.mjdList[0])
+
+            raDeg, decDeg = coordUtils.applyProperMotion(numpy.degrees(ra), numpy.degrees(dec),
+                                                         arcsecFromRadians(pm_ra), arcsecFromRadians(pm_dec),
+                                                         arcsecFromRadians(px), v_rad, mjd=self.mjdList[0])
+
+            self.assertAlmostEqual((ra-raRad)/(ra-numpy.radians(raDeg)), 1.0, 9)
+            self.assertAlmostEqual((dec-decRad)/(dec-numpy.radians(decDeg)), 1.0, 9)
 
 
 
