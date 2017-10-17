2be57cf1a0ec8da614455f039833e980304c1c17
danielsf <scott.f.daniel@gmail.com>
2017-10-17 12:01:21 -0700

    use **2 when calculating rrsq_lim

--- python/lsst/sims/coordUtils/LsstCameraUtils.py
+++ python/lsst/sims/coordUtils/LsstCameraUtils.py
@@ -246,7 +246,7 @@ def chipNameFromPupilCoordsLSST(xPupil, yPupil, allow_multiple_chips=False):
     valid_detectors = []
     x_cam = chipNameFromPupilCoordsLSST._pupil_map['xx']
     y_cam = chipNameFromPupilCoordsLSST._pupil_map['yy']
-    rrsq_lim = np.power(1.1*chipNameFromPupilCoordsLSST._pupil_map['dp'],2)
+    rrsq_lim = (1.1*chipNameFromPupilCoordsLSST._pupil_map['dp'])**2
     for xx, yy in zip(xPupil, yPupil):
         t_before_where = time.time()
         possible_dexes = np.where(((xx-x_cam)**2 + (yy-y_cam)**2) < rrsq_lim)
