873a5ed691dbfc84a723b3c740f36d6190f6456e
danielsf <scott.f.daniel@gmail.com>
2017-10-17 11:42:52 -0700

    try to filter on dx and dy first; that also made it slower

--- python/lsst/sims/coordUtils/LsstCameraUtils.py
+++ python/lsst/sims/coordUtils/LsstCameraUtils.py
@@ -244,11 +244,15 @@ def chipNameFromPupilCoordsLSST(xPupil, yPupil, allow_multiple_chips=False):
     t_before_guess = time.time()
     t_where = 0.0
     valid_detectors = []
+    x_cam = chipNameFromPupilCoordsLSST._pupil_map['xx']
+    y_cam = chipNameFromPupilCoordsLSST._pupil_map['yy']
+    rr_cam = chipNameFromPupilCoordsLSST._pupil_map['dp']
     for xx, yy in zip(xPupil, yPupil):
         t_before_where = time.time()
-        possible_dexes = np.where(np.sqrt(np.power(xx-chipNameFromPupilCoordsLSST._pupil_map['xx'], 2) +
-                                          np.power(yy-chipNameFromPupilCoordsLSST._pupil_map['yy'], 2))/
-                                          chipNameFromPupilCoordsLSST._pupil_map['dp'] < 1.1)
+        possible_dexes = np.where(np.logical_and(np.abs(xx-x_cam)<1.1*rr_cam,
+                                  np.logical_and(np.abs(yy-y_cam)<1.1*rr_cam,
+                                                 np.sqrt(np.power(xx-x_cam, 2) +
+                                                 np.power(yy-y_cam, 2))/rr_cam < 1.1)))
         t_where += time.time()-t_before_where
 
         local_valid = chipNameFromPupilCoordsLSST._detector_arr[possible_dexes]
