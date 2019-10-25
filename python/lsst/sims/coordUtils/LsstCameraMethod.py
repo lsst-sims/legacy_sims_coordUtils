93bf42e08b8e00f12938100a05d11afb17f62347
danielsf <scott.f.daniel@gmail.com>
2019-10-25 10:11:42 -0700

    make lsst_camera() an empty method that returns an error explaining what happened

--- python/lsst/sims/coordUtils/LsstCameraMethod.py
+++ python/lsst/sims/coordUtils/LsstCameraMethod.py
@@ -1,6 +1,3 @@
-from lsst.obs.lsstSim import LsstSimMapper
-import lsst.log as lsstLog
-
 
 __all__ = ["lsst_camera"]
 
@@ -9,8 +6,19 @@ def lsst_camera():
     """
     Return a copy of the LSST Camera model as stored in obs_lsstSim.
     """
-    if not hasattr(lsst_camera, '_lsst_camera'):
-        lsstLog.setLevel('CameraMapper', lsstLog.WARN)
-        lsst_camera._lsst_camera = LsstSimMapper().camera
+    msg = "\n\n"
+    msg += "You are calling sims_coordUtils.lsst_camera().\n"
+    msg += "This used to return a model for the LSST camera based on "
+    msg += "lsst.obs.lsstSim.\n"
+    msg += "lsst.obs.lsstSim is no longer a supported part of the LSST stack.\n"
+    msg += "It has been replaced with lsst.obs.lsst. Because lsst.obs.lsst uses\n"
+    msg += "different coordinate conventions than lsst.obs.lsstSim, it was\n"
+    msg += "decided not to port the LsstCameraUtils functionality over to\n"
+    msg += "using lsst.obs.lsst. If you would still like to use the functionality\n"
+    msg += "provided by sims_coordUtils for mapping sky coordinates to focal plane\n"
+    msg += "coordinates, it is recommended that you use the methods in\n"
+    msg += "lsst.sims.coordUtils.CameraUtils with a camera instantiated as\n\n"
+    msg += "lsst.obs.lsst.phosim.PhosimMapper().camera\n\n"
 
-    return lsst_camera._lsst_camera
+    raise RuntimeError(msg)
+    return None
